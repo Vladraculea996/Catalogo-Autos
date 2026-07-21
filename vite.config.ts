7658a816caf2c3c151eeed9eae71629f30e15c81
Vladraculea996 <158999332+Vladraculea996@users.noreply.github.com>
2026-07-21 11:15:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Catalogo-Autos/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
